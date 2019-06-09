42cf046c064f6c685d81032f83e918b479c4ef61
Kimbugwe Simon Peter <kimbsimon2@gmail.com>
2019-06-09 21:12:45 +0300

    jupyter files

---
+++ main.py
@@ -0,0 +1,15 @@
+#%%
+import calendar
+
+
+def get_leap_years(start: int, stop: int):
+    if start > stop:
+        start, stop = stop, start
+    leap_years = filter(lambda x: calendar.isleap(x), range(start, stop, +1))
+    return list(leap_years)
+
+
+print(get_leap_years(2019, 2018))
+
+
+#%%
