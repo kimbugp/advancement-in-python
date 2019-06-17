03157ad4c75913f9642e4ebb92a460426be46a99
Kimbugwe Simon Peter <kimbsimon2@gmail.com>
2019-06-17 12:34:55 +0300

    connected cities

--- mapper.py
+++ mapper.py
@@ -1,23 +1,16 @@
-#!/bin/python3
-
 import math
-import os
-import random
-import re
-import sys
+from collections import defaultdict
 
 
-def factors(x):
-    l = list()
-    for i in range(1, x + 1):
-        if x % i == 0:
-            l.append(i)
-    return l
+def addEdge(graph, u, v):
+    graph[u].add(v)
 
 
-# for _ in range(5):
-#     x = int(input())
-#     print(factors(x))
+def find_path(graph, start, end):
+    for path in graph[start]:
+        if end in graph[path]:
+            return True
+    return False
 
 
 def connectedCities(n, g, originCities, destinationCities):
@@ -29,46 +22,24 @@ def connectedCities(n, g, originCities, destinationCities):
         originCities (list): [description]
         destinationCities (list): [description]
     """
+    graph = defaultdict(set)
+    all_cities = originCities + destinationCities
+
+    for city in all_cities:
+        for destination in all_cities:
+            divisor = math.gcd(city, destination)
+            if divisor > g:
+                addEdge(graph, city, destination)
     result = list()
     for origin, destination in zip(originCities, destinationCities):
-        divisor = math.gcd(origin, destination)
-        if divisor > g:
+        path = find_path(graph, origin, destination)
+        print(origin, '  ', destination)
+        if path:
             result.append(1)
-            print("Valid route", origin, destination, sep=" ")
         else:
-            print("InValid route", origin, destination, sep=" ")
             result.append(0)
+    print(result)
     return result
 
 
-connectedCities(6, 0, [1, 2, 4, 6], [3, 3, 3, 4])
-# connectedCities(6, 2, [4, 2, 9], [8, 5, 6])
-if __name__ == '__main__':
-    fptr = open(os.environ['OUTPUT_PATH'], 'w')
-
-    n = int(input("Number of cities ").strip())
-
-    g = int(input("Threshold ").strip())
-
-    originCities_count = int(input("Origin Cities count ").strip())
-
-    originCities = []
-
-    for _ in range(originCities_count):
-        originCities_item = int(input().strip())
-        originCities.append(originCities_item)
-
-    destinationCities_count = int(input("Destination city count ").strip())
-
-    destinationCities = []
-
-    for _ in range(destinationCities_count):
-        destinationCities_item = int(input().strip())
-        destinationCities.append(destinationCities_item)
-
-    result = connectedCities(n, g, originCities, destinationCities)
-
-    # fptr.write('\n'.join(map(str, result)))
-    # fptr.write('\n')
-
-    # fptr.close()
+connectedCities(10, 1, [10, 4, 3, 6], [3, 6, 2, 9])
