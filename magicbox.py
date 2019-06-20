c3711b46c4c3d4de31e5dc572f4318a24e9514b5
Kimbugwe Simon Peter <kimbsimon2@gmail.com>
2019-06-20 11:33:39 +0300

    magic box

--- magicbox.py
+++ magicbox.py
@@ -1,32 +1,13 @@
 from functools import reduce
 from itertools import combinations, permutations
 
+#%% [markdown]
+
+#### Find the lowest steps to turn a 3x3 square into a magic square
+#%%
+
 
 def formingMagicSquare(square):
-    K = 15
-    n = 3
-    pivot = 5
-    center = [1, 5, 9, 3, 7]
-    sample = list(combinations(range(1, n**2 + 1), n))
-    # sample = list(permutations(range(1, n**2 + 1), n))
-    possible = list(
-        filter(
-            lambda x: sum(x) == K and x[n // 2] in center and x[0] != pivot and
-            x[-1] != pivot, sample))
-    import pdb; pdb.set_trace()
-    center_pieces = list(filter(lambda x: x[n // 2] == pivot, possible))
-    for _ in center_pieces:
-        possible.remove(_)
-    # remove losing centers
-    center_pieces = list(
-        filter(lambda x: x[0] not in [2, 4, 6, 8] or x[-1] not in [2, 4, 6, 8],
-               center_pieces))
-
-    tries = []
-    for center_piece in center_pieces:
-        import pdb
-        pdb.set_trace()
-
     possible = [[[8, 1, 6], [3, 5, 7], [4, 9, 2]],
                 [[6, 1, 8], [7, 5, 3], [2, 9, 4]],
                 [[4, 9, 2], [3, 5, 7], [8, 1, 6]],
