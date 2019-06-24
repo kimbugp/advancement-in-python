09474f2d1b8aa0c0d3bbf3388a0590b69ca27ca4
Kimbugwe Simon Peter <kimbsimon2@gmail.com>
2019-06-24 10:55:07 +0300

    scripting

---
+++ branch_rename.sh
@@ -0,0 +1,40 @@
+#!/bin/bash
+
+helpFunction()
+{
+   echo "Function usage"
+   echo "Usage: rename [oldname]  [newname] "
+   echo -e "\tOld name of the branch"
+   echo -e "\tNew name of the branch"
+}
+
+rename(){
+    RED='\033[0;31m'
+    NONE='\033[0m'
+    GREEN='\033[0;32m'    
+    if [ -z "$1" ] || [ -z "$2" ]
+    then
+        helpFunction
+        return
+    fi
+    if git show-ref -q --heads $1 && !git show-ref -q --heads $2; then
+        git branch -m $1 $2
+        git push origin :$1 $2
+        git push origin -u $2
+        echo -e "${GREEN}Rename complete${NONE}"
+    fi
+    echo -e "Invalid branch name ${RED}$1${NONE} and ${RED}$2${NONE}"
+}
+
+push(){
+    branch=$(git rev-parse --symbolic-full-name --abbrev-ref HEAD)
+    if [ "$1" = "-f" ]; then
+        echo "Good luck"
+        git add . && git commit --amend --no-edit &&git push -f
+    elif [ "$branch" != "develop" ];then
+        echo 'Pushing to branch' $branch 
+        git add . && git commit --amend --no-edit &&git push -f
+    else
+        echo 'Boss this is develop\nBe serious '
+    fi
+}
--- startup.bash
+++
@@ -1,3 +0,0 @@
-python manage.py bower install
-python manage.py migrate
-invoke load-fixtures
