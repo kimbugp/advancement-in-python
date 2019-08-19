2f4d1ad725eadebc68d19388f2e2f1efaad8fd3b
Kimbugwe Simon Peter <kimbsimon2@gmail.com>
2019-08-19 11:30:46 +0300

    add graph

--- graph.py
+++ graph.py
@@ -8,7 +8,7 @@ class Graph(object):
             graph_dict = defaultdict(set)
         self.__graph_dict = graph_dict
 
-    def vertices(self):
+    def nodes(self):
         return list(self.__graph_dict.keys())
 
     def edges(self):
@@ -19,57 +19,65 @@ class Graph(object):
 
     def __generate_edges(self):
         edges = []
-        for vertex in self.__graph_dict:
-            for neighbour in self.__graph_dict[vertex]:
-                if {neighbour, vertex} not in edges:
-                    edges.append({vertex, neighbour})
+        for node in self.__graph_dict:
+            for neighbour in self.__graph_dict[node]:
+                if {neighbour, node} not in edges:
+                    edges.append({node, neighbour})
         return edges
 
-    def find_path(self, start_vertex, end_vertex, path=[]):
+    def find_path(self, start_node, end_node, path=[]):
         graph = self.__graph_dict
-        path = path + [start_vertex]
-        if start_vertex == end_vertex:
+        path = path + [start_node]
+        if start_node == end_node:
             return path
-        if start_vertex not in graph:
+        if start_node not in graph:
             return None
-        for vertex in graph[start_vertex]:
-            if vertex not in path:
-                extended_path = self.find_path(vertex, end_vertex, path)
+        for node in graph[start_node]:
+            if node not in path:
+                extended_path = self.find_path(node, end_node, path)
                 if extended_path:
                     return extended_path
         return None
 
-    def find_shortest_path(self, start_vertex, end_vertex, path=[]):
+    def find_shortest_path(self, start_node, end_node, path=[]):
         graph = self.__graph_dict
-        path = path + [start_vertex]
-        if start_vertex == end_vertex:
+        path = path + [start_node]
+        if start_node == end_node:
             return path
-        if start_vertex not in graph:
+        if start_node not in graph:
             return None
 
         shortest = None
-        for vertex in graph[start_vertex]:
-            if vertex not in path:
+        for node in graph[start_node]:
+            if node not in path:
                 extended_path = self.find_shortest_path(
-                    vertex, end_vertex, path)
+                    node, end_node, path)
                 if extended_path:
                     if not shortest or len(extended_path) < len(shortest):
                         shortest = extended_path
         return shortest
 
     def breadthfirstsearch(self, startnode):
+        """
+        BFS involves searching a node and its siblings
+        before going on to any children
+        """
         # Track the visited and unvisited nodes
-        # uses a queue 
+        # uses a queue
         seen, queue = set([startnode]), collections.deque([startnode])
         while queue:
-            vertex = queue.popleft()
-            for node in self.__graph_dict[vertex]:
+            node = queue.popleft()
+            for node in self.__graph_dict[node]:
                 if node not in seen:
                     seen.add(node)
                     queue.append(node)
         return seen
 
     def depth_first_search(self, start, visited=None):
+        """
+        DFS involves searching a node and all its children
+        before proceeding to its siblings
+        """
         # uses a stack `visited`
         if visited is None:
             visited = set()
@@ -79,7 +87,7 @@ class Graph(object):
         return visited
 
     def __repr__(self):
-        res = "vertices: "
+        res = "nodes: "
         for k in self.__graph_dict:
             res += str(k) + " "
         res += "\nedges: "
@@ -100,7 +108,7 @@ graph_dict = {
 
 g = Graph(graph_dict)
 
-## gets all nodes that have been traversed
+# gets all nodes that have been traversed
 nodes_1 = g.breadthfirstsearch("f")
 nodes_2 = g.depth_first_search("f")
 
